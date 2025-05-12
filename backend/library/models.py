9273917a358fffe45c2f7348e58fd835cf3f5532
Tauya Mtowodzwa <tauyamtowodzwa@gmail.com>
2025-05-12 10:15:48 +0200

    update

---
+++ backend/library/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+    quantity = models.PositiveIntegerField(default=1)
+    available = models.PositiveIntegerField(default=1)
+    
+    def __str__(self):
+        return self.title
+
+class BorrowRecord(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    borrow_date = models.DateTimeField(auto_now_add=True)
+    return_date = models.DateTimeField(null=True, blank=True)
+    
+    def __str__(self):
+        return f"{self.user.username} - {self.book.title}"
